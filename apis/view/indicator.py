1e562e3aee6da848a11b6f624cef6626aee5d059
npuNancy <1575812469@qq.com>
2022-11-05 20:12:39 +0800

    修改API：获取指标

--- apis/view/indicator.py
+++ apis/view/indicator.py
@@ -1,8 +1,10 @@
+import os
 import xlrd
 import json
-from common.base.base_respons import retJson
+from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 
+from common.base.base_respons import retJson
 
 @csrf_exempt
 def add_indicators(request):
@@ -69,66 +71,30 @@ def add_indicators(request):
 
 
 def system1_dataget(filename):
-    data = []
-    excel_data = xlrd.open_workbook(filename=filename)
+    data = []  # 一级指标列表
+    excel_data = xlrd.open_workbook(filename=filename, formatting_info=True)
     table = excel_data.sheets()[0]
     merge = table.merged_cells
     for tupe0 in merge:
         if tupe0[2] == 0 and tupe0[3] == 1:
             topic_temp = {}  # 指标主题和需求目的
-            indicators_list = []
             topic_temp["指标主题"] = table.cell_value(tupe0[0], tupe0[2])
             topic_temp["需求目的"] = table.cell_value(tupe0[0], tupe0[2] + 1)
+            indicators_list = []  # 二级指标列表
             for tupe1 in merge:
                 if tupe1[2] == 2 and tupe1[3] == 3 and tupe1[0] >= tupe0[0] and tupe1[1] <= tupe0[1]:
-                    indicators_temp = {}  # 具体指标
-                    last_indicator_list = []
+                    indicators_temp = {}  # 具体指标/二级指标
                     indicators_temp["具体指标名称"] = table.cell_value(tupe1[0], tupe1[2])
+                    last_indicator_list = []  # 三级指标列表
                     for index in range(tupe1[0], tupe1[1]):
                         last_indicator_temp = {}  # 三级指标
                         last_indicator_temp["name"] = table.cell_value(index, tupe1[2] + 1)
                         last_indicator_temp["keywords"] = table.cell_value(index, tupe1[2] + 2)
                         last_indicator_list.append(last_indicator_temp)
-                    # print(last_indicator_list)
                     indicators_temp["三级指标"] = last_indicator_list
                     indicators_list.append(indicators_temp)
             topic_temp["具体指标"] = indicators_list
-            # print(topic_temp)
             data.append(topic_temp)
-            # print(data)
-    print(data)
-    return data
-
-
-def system1_dataget(filename):
-    data = []
-    excel_data = xlrd.open_workbook(filename=filename)
-    table = excel_data.sheets()[0]
-    merge = table.merged_cells
-    for tupe0 in merge:
-        if tupe0[2] == 0 and tupe0[3] == 1:
-            topic_temp = {}  # 指标主题和需求目的
-            indicators_list = []
-            topic_temp["指标主题"] = table.cell_value(tupe0[0], tupe0[2])
-            topic_temp["需求目的"] = table.cell_value(tupe0[0], tupe0[2] + 1)
-            for tupe1 in merge:
-                if tupe1[2] == 2 and tupe1[3] == 3 and tupe1[0] >= tupe0[0] and tupe1[1] <= tupe0[1]:
-                    indicators_temp = {}  # 具体指标
-                    last_indicator_list = []
-                    indicators_temp["具体指标名称"] = table.cell_value(tupe1[0], tupe1[2])
-                    for index in range(tupe1[0], tupe1[1]):
-                        last_indicator_temp = {}  # 三级指标
-                        last_indicator_temp["name"] = table.cell_value(index, tupe1[2] + 1)
-                        last_indicator_temp["keywords"] = table.cell_value(index, tupe1[2] + 2)
-                        last_indicator_list.append(last_indicator_temp)
-                    # print(last_indicator_list)
-                    indicators_temp["三级指标"] = last_indicator_list
-                    indicators_list.append(indicators_temp)
-            topic_temp["具体指标"] = indicators_list
-            # print(topic_temp)
-            data.append(topic_temp)
-            # print(data)
-    print(data)
     return data
 
 
@@ -171,9 +137,9 @@ def indicators(request):
         system = int(request.GET.get('system'))
         res = []
         if system == 1:
-            filename = r'D:\作业\研究生\研1\CarbonInfoSystem\data\副本碳信息披露质量指标体系.xlsx'
+            filename = os.path.join(settings.BASE_DIR, "data\碳信息披露质量指标体系.xls")
             res = system1_dataget(filename)
-            return retJson(code=1, msg="success", data={"indicators": json.dumps(res, ensure_ascii=True)})
+            return retJson(code=1, msg="success", data={"indicators": res})
         elif system == 2:
 
             return retJson(code=1, msg="success", data={"indicators": res})
